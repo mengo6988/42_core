4b206b99c3ee011e7fe7889ae568a36e88c48730
mengo <menghong6988@gmail.com>
2024-02-01 18:37:29 +0800

    start bonus

--- philosophers/srcs/main.c
+++ philosophers/srcs/main.c
@@ -12,22 +12,19 @@
 
 #include "philo.h"
 
-int	check_args(char **av)
-{
-	int	i;
-	int	j;
+int check_args(char **av) {
+  int i;
+  int j;
 
-	i = -1;
-	while (av[++i])
-	{
-		j = -1;
-		while (av[i][++j])
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
+  i = -1;
+  while (av[++i]) {
+    j = -1;
+    while (av[i][++j]) {
+      if (av[i][j] < '0' || av[i][j] > '9')
+        return (1);
+    }
+  }
+  return (0);
 }
 
 // void	test_philos(t_data *data)
@@ -43,18 +40,17 @@ int	check_args(char **av)
 // 		printf("philos eating= %i\n", data->philos[i].eating);
 // 		printf("philos meals_eaten= %i\n", data->philos[i].meals_eaten);
 // 		printf("philos lastmeal= %zu\n", data->philos[i].last_meal);
-// 		printf("philos time to die= %zu\n", data->philos[i].time_to_die);
-// 		printf("philos time to eat= %zu\n", data->philos[i].time_to_eat);
-// 		printf("philos time to sleep= %zu\n", data->philos[i].time_to_sleep);
-// 		printf("philos time start= %zu\n", data->philos[i].start_time);
-// 		if (i == data->philos[i].number_of_philos - 1)
+// 		printf("philos time to die= %zu\n",
+// data->philos[i].time_to_die); 		printf("philos time to eat= %zu\n",
+// data->philos[i].time_to_eat); 		printf("philos time to sleep= %zu\n",
+// data->philos[i].time_to_sleep); 		printf("philos time start= %zu\n",
+// data->philos[i].start_time); 		if (i == data->philos[i].number_of_philos - 1)
 // 			printf("data chopsticks (%p) vs right chopstick (%p)\n",
-// 				&data->chopsticks[0], data->philos[i].right_chopstick);
-// 		else
-// 			printf("data chopsticks (%p) vs right chopstick (%p)\n",
-// 				&data->chopsticks[i + 1], data->philos[i].right_chopstick);
-// 		printf("data chopsticks (%p) vs left chopstick (%p)\n",
-// 			&data->chopsticks[i], data->philos[i].left_chopstick);
+// 				&data->chopsticks[0],
+// data->philos[i].right_chopstick); 		else 			printf("data chopsticks (%p) vs right
+// chopstick (%p)\n", 				&data->chopsticks[i + 1],
+// data->philos[i].right_chopstick); 		printf("data chopsticks (%p) vs left
+// chopstick (%p)\n", 			&data->chopsticks[i], data->philos[i].left_chopstick);
 // 		printf("philos number of philos= %u\n",
 // 			data->philos[i].number_of_philos);
 // 		printf("philos number of times to eat = %i\n",
@@ -70,15 +66,14 @@ int	check_args(char **av)
 // 	}
 // }
 
-int	main(int ac, char **av)
-{
-	t_data	data;
+int main(int ac, char **av) {
+  t_data data;
 
-	if (ac < 5 || ac > 6 || !check_args(av))
-		return (1);
-	data_init(&data, av);
-	philo_init(data.philos, &data, ac, av);
-	thread_init(&data);
-	ft_exit(&data, NULL);
-	return (0);
+  if (ac < 5 || ac > 6 || !check_args(av))
+    return (1);
+  data_init(&data, av);
+  philo_init(data.philos, &data, ac, av);
+  thread_init(&data);
+  ft_exit(&data, NULL);
+  return (0);
 }
